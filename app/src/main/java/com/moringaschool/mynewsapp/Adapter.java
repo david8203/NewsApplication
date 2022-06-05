62abd957a549e0eb894af7807452681c786ba55b
David <kingdice@Atomic>
2022-06-06 01:04:47 +0300

    here we add some logic in our main activity

---
+++ app/src/main/java/com/moringaschool/mynewsapp/Adapter.java
@@ -0,0 +1,4 @@
+package com.moringaschool.mynewsapp;
+
+public class Adapter {
+}
--- app/src/main/java/com/moringaschool/mynewsapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/MainActivity.java
@@ -1,14 +1,108 @@
 package com.moringaschool.mynewsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.moringaschool.mynewsapp.api.ApiClient;
+import com.moringaschool.mynewsapp.api.ApiInterface;
+import com.moringaschool.mynewsapp.models.Article;
+import com.moringaschool.mynewsapp.models.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
+    public static final String API_KEY = "57b300334e5f4db09040bfd047e31380";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private RecyclerView.Adapter adapter;
+    private String TAG = MainActivity.class.getSimpleName();
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private TextView topHeadLines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+
+        topHeadLines = findViewById(R.id.topheadlines);
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        onLoadingSwipeRefresh("");
     }
+    public void LoadJson(final String keyword){
+
+        swipeRefreshLayout.setRefreshing(true);
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        String country = Utils.getCountry();
+        String language = Utils.getLanguage();
+
+        Call<News> call;
+
+        if (keyword.length() > 0){
+            call = apiInterface.getNewsSearch(keyword, language,"publishedAt", API_KEY);
+        }else{
+            call = apiInterface.getNews(country, API_KEY);
+        }
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticle()!=null){
+                    if (!articles.isEmpty()){
+                        articles.clear();
+                    }
+                    articles = response.body().getArticle();
+                    adapter = new Adapter(articles, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                    initListener();
+
+                    swipeRefreshLayout.setRefreshing(false);
+
+
+                }else {
+                    swipeRefreshLayout.setRefreshing(false);
+                    Toast.makeText(MainActivity.this, "No Result", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void initListener() {
+    }
+
 }
