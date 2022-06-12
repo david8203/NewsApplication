23a0c8bdfc637e14a14a742545114e3d6a0d6719
David <kingdice@Atomic>
2022-06-13 00:20:02 +0300

    here we write out logic to login after registration process by having options to log in to our application.

--- app/src/main/java/com/moringaschool/mynewsapp/LoginActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/LoginActivity.java
@@ -1,4 +1,58 @@
 package com.moringaschool.mynewsapp;
 
-public class LoginActivity {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.paperdb.Paper;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText InputPhoneNumber, InputPassword;
+    private Button LoginButton, googleBtn, fbBtn;
+    private ProgressDialog loadingBar;
+    private TextView directReg;
+
+    private String parentDbName = "Users";
+    private CheckBox chkBoxRememberMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        directReg = (TextView)findViewById(R.id.direct_reg);
+
+        directReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        LoginButton = (Button) findViewById(R.id.login_btn);
+        googleBtn = (Button)findViewById(R.id.sigin_google);
+        fbBtn = (Button) findViewById(R.id.sigin_fb);
+        InputPassword = (EditText) findViewById(R.id.login_password_input);
+        InputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input);
+        loadingBar = new ProgressDialog(this);
+
+        chkBoxRememberMe = (CheckBox) findViewById(R.id.remember_me_chkb);
+        Paper.init(this);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginUser();
+            }
+        });
+    }
+
 }
