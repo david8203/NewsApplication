9442c6511121ac61d10378fab8b016fb21f1c80c
David <kingdice@Atomic>
2022-06-13 01:11:58 +0300

    here we add some logic in our  login activity to sign a user in and also confirm whether he or she is already registered, and also checks on all the credentials.

--- app/src/main/java/com/moringaschool/mynewsapp/LoginActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/LoginActivity.java
@@ -3,13 +3,24 @@ package com.moringaschool.mynewsapp;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.moringaschool.mynewsapp.activities.MainActivity;
+import com.moringaschool.mynewsapp.models.Users;
 
 import io.paperdb.Paper;
 
@@ -53,6 +64,84 @@ public class LoginActivity extends AppCompatActivity {
                 LoginUser();
             }
         });
+    }
+    private void LoginUser()
+    {
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(phone))
+        {
+            Toast.makeText(this,"please write your phone number..",Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this,"please write your password..",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Login Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            AllowAccessToAccount(phone, password);
+        }
+    }
+
+
+    private void AllowAccessToAccount(final String phone, final String password)
+    {
+        if (chkBoxRememberMe.isChecked())
+        {
+            Paper.book().write(Prevalent.UserPhoneKey, phone);
+            Paper.book().write(Prevalent.UserPasswordKey, password);
+        }
+
+
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.child(parentDbName).child(phone).exists())
+                {
+                    Users usersData = dataSnapshot.child(parentDbName).child(phone).getValue(Users.class);
+
+                    if (usersData.getPhone().equals(phone))
+                    {
+                        if (usersData.getPassword().equals(password))
+                        {
+                            Toast.makeText(LoginActivity.this, "logged in Successfully...", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            Prevalent.currentOnlineUser = usersData;
+                            startActivity(intent);
+                        }
+                        else
+                        {
+                            loadingBar.dismiss();
+                            Toast.makeText(LoginActivity.this, "Password is incorrect.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Account with this " + phone + " number do not exists.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
---
+++ app/src/main/java/com/moringaschool/mynewsapp/Prevalent.java
@@ -0,0 +1,11 @@
+package com.moringaschool.mynewsapp;
+
+import com.moringaschool.mynewsapp.models.Users;
+
+public class Prevalent {
+    // using it for forgotten password, remembering me etc
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
