ec7df996eae61fff33d9abf32bbbe11186cff240
David <kingdice@Atomic>
2022-06-13 00:09:06 +0300

    here we declare all our necessary views in our registration activity.

--- app/src/main/java/com/moringaschool/mynewsapp/RegistrationActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/RegistrationActivity.java
@@ -1,4 +1,35 @@
 package com.moringaschool.mynewsapp;
 
-public class RegistrationActivity {
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationActivity extends AppCompatActivity {
+    private Button CreateAccountButton;
+    private EditText InputName, InputPhoneNumber, InputPassword;
+    private ProgressDialog loadingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        CreateAccountButton = (Button)findViewById(R.id.register_btn);
+        InputName =(EditText)findViewById(R.id.register_user_name_input);
+        InputPassword =(EditText)findViewById(R.id.register_password_input);
+        InputPhoneNumber =(EditText)findViewById(R.id.register_phone_number_input);
+        loadingBar = new ProgressDialog(this);
+
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateAccount();
+            }
+        });
+    }
 }
