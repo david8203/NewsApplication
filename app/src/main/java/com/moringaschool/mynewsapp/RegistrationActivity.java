485f8d036ee287f2e910832bbf1dcdf600291a2f
David <kingdice@Atomic>
2022-06-13 00:11:25 +0300

    here we create the logic to create a new account and apply toasts to show the user the progress.

--- app/src/main/java/com/moringaschool/mynewsapp/RegistrationActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/RegistrationActivity.java
@@ -2,9 +2,11 @@ package com.moringaschool.mynewsapp;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,4 +34,33 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
     }
+    private void CreateAccount(){
+        String name = InputName.getText().toString();
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(name))
+        {
+            Toast.makeText(this,"please write your name..",Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(phone))
+        {
+            Toast.makeText(this,"please write your phone number..",Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this,"please write your password..",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            ValidatephoneNumber(name, phone, password);
+        }
+
+    }
+
 }
