ca3772c7727fde307fd3dcb4ebbe6db03f34fe05
David <kingdice@Atomic>
2022-06-13 00:15:19 +0300

    here we try validating the user phone number and utilizes the hashmaps to save data to firebase and also checking whether the user has already created an account.

--- app/src/main/java/com/moringaschool/mynewsapp/RegistrationActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.moringaschool.mynewsapp;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -8,7 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.moringaschool.mynewsapp.activities.MainActivity;
+
+import java.util.HashMap;
 
 public class RegistrationActivity extends AppCompatActivity {
     private Button CreateAccountButton;
@@ -61,6 +74,61 @@ public class RegistrationActivity extends AppCompatActivity {
             ValidatephoneNumber(name, phone, password);
         }
 
+    }
+    private void ValidatephoneNumber(final String name, final String phone, final String password)
+    {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!(dataSnapshot.child("Users").child(phone).exists()))
+                {
+                    //hashmap for put data in firebase databse
+                    HashMap<String, Object> userdataMap = new HashMap<>();
+                    userdataMap.put("phone", phone);
+                    userdataMap.put("password", password);
+                    userdataMap.put("name", name);
+
+                    RootRef.child("Users").child(phone).updateChildren(userdataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(RegistrationActivity.this,"Congrats, your account has been created. ", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+
+                                        Intent intent = new Intent(RegistrationActivity.this,LoginActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    else
+                                    {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(RegistrationActivity.this,"Network error, please try after some time.", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                }
+                else
+                {
+                    Toast.makeText(RegistrationActivity.this,"This " + phone + "already exists.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+
+                    Toast.makeText(RegistrationActivity.this,"Please try agian using another phone number.",Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
