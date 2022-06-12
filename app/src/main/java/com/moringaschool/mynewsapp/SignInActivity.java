72c05e37a9c967b06a2f1959f2b5f4c8980b2727
David <kingdice@Atomic>
2022-06-12 23:06:24 +0300

    here we are adding the logic behind our sign up main class and passing it to our register activity

--- app/src/main/java/com/moringaschool/mynewsapp/SignInActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/SignInActivity.java
@@ -1,4 +1,27 @@
 package com.moringaschool.mynewsapp;
 
-public class SignInActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+    TextView signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        signIn = findViewById(R.id.signIn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignInActivity.this,OtpRegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 }
