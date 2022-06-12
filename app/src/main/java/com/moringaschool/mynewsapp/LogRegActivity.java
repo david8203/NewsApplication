6edd009e31afde0fdb528b8b7675cd26cf3d3e32
David <kingdice@Atomic>
2022-06-13 00:31:03 +0300

    here we are creating a log registration class that will take us to the login activity by an intent.

---
+++ app/src/main/java/com/moringaschool/mynewsapp/LogRegActivity.java
@@ -0,0 +1,38 @@
+package com.moringaschool.mynewsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogRegActivity extends AppCompatActivity {
+    private Button joinBtn, logRegBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_reg);
+
+        joinBtn = findViewById(R.id.joinNowBtn);
+        logRegBtn = findViewById(R.id.logOregBtn);
+
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LogRegActivity.this, RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        logRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LogRegActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
