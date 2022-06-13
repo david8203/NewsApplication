521afaaf0d10308ec3bdd0d6f9fd266993b26d95
David <kingdice@Atomic>
2022-06-13 09:55:00 +0300

    here we create a one time passeword class which will make sure our user data is stored successfully in the firebase database.

---
+++ app/src/main/java/com/moringaschool/mynewsapp/OtpActivity.java
@@ -0,0 +1,71 @@
+package com.moringaschool.mynewsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.moringaschool.mynewsapp.activities.MainActivity;
+
+public class OtpActivity extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore fStore;
+    TextView pName,pEmail,pPhone;
+    String mName,mEmail,mPhone;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Profile");
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(OtpActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        pName = findViewById(R.id.profileFullName);
+        pEmail = findViewById(R.id.profileEmail);
+        pPhone = findViewById(R.id.profilePhone);
+
+        DocumentReference docRef = fStore.collection("users").document(firebaseAuth.getCurrentUser().getUid());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    String fullName = documentSnapshot.getString("firstName") + " " + documentSnapshot.getString("lastName");
+                    pName.setText(fullName);
+                    pEmail.setText(documentSnapshot.getString("emailAddress"));
+                    pPhone.setText(firebaseAuth.getCurrentUser().getPhoneNumber());
+
+                }else {
+//                    Log.d(TAG, "Retrieving Data: Profile Data Not Found ");
+                }
+            }
+        });
+    }
+}
