d3a751d0e53232e7bdb75d0cc83886b0c306eb20
David <kingdice@Atomic>
2022-06-13 12:41:29 +0300

    here we create an if loop to check whether the user meets all the requirements and if not so the account is not created.

--- app/src/main/java/com/moringaschool/mynewsapp/OtpRegisterActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/OtpRegisterActivity.java
@@ -1,5 +1,6 @@
 package com.moringaschool.mynewsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,14 +8,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.moringaschool.mynewsapp.activities.MainActivity;
 import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
 
 public class OtpRegisterActivity extends AppCompatActivity {
     public static final String TAG = "TAG";
@@ -72,6 +85,81 @@ public class OtpRegisterActivity extends AppCompatActivity {
                         codeEnter.setError("Valid OTP is required.");
                     }
                 }
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (fAuth.getCurrentUser() != null){
+            progressBar.setVisibility(View.VISIBLE);
+            state.setText("Checking..");
+            state.setVisibility(View.VISIBLE);
+            checkUserProfile();
+        }
+    }
+
+
+    private void verifyAuth(PhoneAuthCredential credential) {
+        fAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    checkUserProfile();
+                }else {
+                    Toast.makeText(OtpRegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void checkUserProfile() {
+        DocumentReference docRef = fStore.collection("users").document(fAuth.getCurrentUser().getUid());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()){
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                }else {
+                    startActivity(new Intent(getApplicationContext(),OtpDetailsActivity.class));
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void requestOTP(String phoneNum) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNum, 60L, TimeUnit.SECONDS, this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                progressBar.setVisibility(View.GONE);
+                state.setVisibility(View.GONE);
+                codeEnter.setVisibility(View.VISIBLE);
+                verificationId = s;
+                token = forceResendingToken;
+                nextBtn.setText("Verify");
+                verificationInProgress = true;
+
+            }
+
+            @Override
+            public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+                super.onCodeAutoRetrievalTimeOut(s);
+                Toast.makeText(OtpRegisterActivity.this, "OTP Expired , Re - request the OTP.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                verifyAuth(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(OtpRegisterActivity.this, "cannot Create Account" + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
