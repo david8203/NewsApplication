6244c1932af06aade336314f68816bc41bf00f69
David <kingdice@Atomic>
2022-06-17 11:54:59 +0300

    here we create two instances one for the animation and one for the image view.

--- app/src/main/java/com/moringaschool/mynewsapp/SplashActivity.java
+++ app/src/main/java/com/moringaschool/mynewsapp/SplashActivity.java
@@ -3,12 +3,18 @@ package com.moringaschool.mynewsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
-
+    Animation anim;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        imageView=(ImageView)findViewById(R.id.imageView2); // Declares the imageView to show the animation.
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in); // Create the animation.
     }
 }
